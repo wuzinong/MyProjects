70b4ea76b45333d1cbd3876db759eaa22ef44d57
Gu <Xie.Hai.Bran.Gu@dnvgl.com>
2018-03-26 12:30:15 +0800

    update for confirmation page

--- Ecommerce/src/views/confirmation/confirmation-component.js
+++ Ecommerce/src/views/confirmation/confirmation-component.js
@@ -16,7 +16,7 @@ class Confirmation extends Component{
                    <p>You will also recieve an email confirmation with your order invoice.</p>
                </div>
                <h2>Your Order Information</h2>
-               <div className="row">
+               <div className="row space-stack-md">
                     <div className="col-sm-7 col-xs-12">
                         <div className="order-box order-box-mb space-inset-sm">
                             <ul>
@@ -43,7 +43,7 @@ class Confirmation extends Component{
                                     <p>For any concerns that you might have, please contact us.</p>
                                 </div>
                                 </a>
-                                <a href="https://mytest.dnvgl.com/FAQ?loggedin=1" className="order-support-item clearfix faq">
+                                <a href="javascript:void(0)" className="order-support-item clearfix faq">
                                 <div className="order-support-image">
                                     <img className="order-icon" src={require("../../assets/images/question.png")} alt=""/>
                                 </div>
@@ -56,6 +56,37 @@ class Confirmation extends Component{
                         </div>
                     </div>
                </div>
+
+               <div className="row confirmationInformation space-stack-md">
+                    <div className="row col-12 nopadding">
+                        <div className="col-sm-6">                
+                            <ul>
+                                <li><b>Your company information</b></li>
+                                <li><strong>Order number:</strong> <span data-bind="text: $data.id">o321982</span></li>
+                                <li><strong>Order amount:</strong> <span data-bind="currency: {price: $data.priceInfo.amount, currencyObj: $data.priceListGroup.currency}">€0.00</span></li>
+                                <li><strong>VAT:</strong> <span data-bind="currency: {price: $data.priceInfo.tax, currencyObj: $data.priceListGroup.currency}">€0.00</span></li>
+                                <li><strong>Order description:</strong> <span data-bind="text: $data.shoppingCart.items[0].displayName">Automation Test Product 3 (InvoicePayment)</span></li>
+                                <li><strong>Buyer:</strong> <span data-bind="text: $data.shippingAddress.firstName">Bran</span>&nbsp;<span data-bind="text: $data.shippingAddress.lastName">Gu</span></li>
+                                <li><strong>Contact mail:</strong> <span data-bind="text: $data.shippingAddress.email">wuzinong@test.com</span></li>
+                                <li><strong>Mobile phone number:</strong> <span data-bind="text: $data.shippingAddress.phoneNumber">+8611111111111</span></li>
+                            </ul>
+                        </div>
+                        <div className="col-sm-6">
+                            <ul>
+                                <li><b>Your billing information</b></li>
+                                <li><strong>Order number:</strong> <span data-bind="text: $data.id">o321982</span></li>
+                                <li><strong>Order amount:</strong> <span data-bind="currency: {price: $data.priceInfo.amount, currencyObj: $data.priceListGroup.currency}">€0.00</span></li>
+                                <li><strong>VAT:</strong> <span data-bind="currency: {price: $data.priceInfo.tax, currencyObj: $data.priceListGroup.currency}">€0.00</span></li>
+                                <li><strong>Order description:</strong> <span data-bind="text: $data.shoppingCart.items[0].displayName">Automation Test Product 3 (InvoicePayment)</span></li>
+                                <li><strong>Buyer:</strong> <span data-bind="text: $data.shippingAddress.firstName">Bran</span>&nbsp;<span data-bind="text: $data.shippingAddress.lastName">Gu</span></li>
+                                <li><strong>Contact mail:</strong> <span data-bind="text: $data.shippingAddress.email">wuzinong@test.com</span></li>
+                                <li><strong>Mobile phone number:</strong> <span data-bind="text: $data.shippingAddress.phoneNumber">+8611111111111</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <h2>Your order summary</h2>
             </div>
         )
     }
