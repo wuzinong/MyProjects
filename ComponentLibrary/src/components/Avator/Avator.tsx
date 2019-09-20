c03c2c0075e5de2548d4536cb2467239f10f0a3a
wuzinong <wuzinong@gmail.com>
2019-09-20 10:57:47 +0800

    update

---
+++ ComponentLibrary/src/components/Avator/Avator.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {IAvatorProps} from './Avator.types';
+
+let style = require('./Avator.scss');
+
+
+const Avator = (props:IAvatorProps) =>{
+    return <div className={style['avator-wrapper']}>
+                To be done
+        </div>
+ }
+ 
+ export default Avator;
