fef4db3d6c1bf46750680637ad0d287949c1f16d
Gu <Xie.Hai.Bran.Gu@dnv.com>
2022-11-28 14:19:35 +0800

    update

---
+++ BRAN-REACT/packages/react/src/jsx.ts
@@ -0,0 +1,65 @@
+import { REACT_ELEMENT_TYPE } from "shared/ReactSymbols";
+import {
+  Type,
+  Key,
+  Ref,
+  Props,
+  ReactElementType,
+  ElementType,
+} from "shared/ReactTypes";
+
+//ReactElment
+const ReactElement = function (
+  type: Type,
+  key: Key,
+  ref: Ref,
+  props: Props
+): ReactElementType {
+  const element = {
+    $$typeof: REACT_ELEMENT_TYPE,
+    type,
+    key,
+    ref,
+    props,
+    __mark: "Bran",
+  };
+  return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...maybChildren: any) => {
+  let key: Key = null;
+  const props: Props = {};
+  let ref: Ref = null;
+
+  for (const prop in config) {
+    const val = config[prop];
+    if (prop === "key") {
+      if (val !== undefined) {
+        key = "" + val;
+        continue;
+      }
+    }
+    if (prop === "ref") {
+      if (val !== undefined) {
+        ref = val;
+      }
+      continue;
+    }
+    if ({}.hasOwnProperty.call(config, prop)) {
+      props[prop] = val;
+    }
+  }
+
+  const maybeChildrenLength = maybChildren.length;
+  if (maybeChildrenLength) {
+    //children:两种情况，只有一个child ，或者有多个
+    if (maybeChildrenLength === 1) {
+      props.children = maybChildren[0];
+    } else {
+      props.children = maybChildren;
+    }
+  }
+  return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = jsx;
---
+++ BRAN-REACT/packages/shared/ReactTypes.ts
@@ -0,0 +1,14 @@
+export type Type = any;
+export type Key = any;
+export type Ref = any;
+export type Props = any;
+export type ElementType = any;
+
+export interface ReactElementType {
+  $$typeof: symbol | number;
+  type: ElementType;
+  key: KeyboardEvent;
+  props: Props;
+  ref: ReferenceError;
+  __mark: string;
+}
